c116329feb44d51a0fbe3effad3a366f87053cf8
root <root@localhost.localdomain>
2020-01-19 15:07:46 +0530

    Connecting to MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,19 +1,30 @@
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://test:test@cluster0-867kw.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true });
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+var Todo = mongoose.model('Todo', todoSchema);
 
-var data =[{item: 'Get Milk'}, {item: 'Walk Dog'}, {item: 'Finish Hacking John'}];
+//var data =[{item: 'Get Milk'}, {item: 'Walk Dog'}, {item: 'Finish Hacking John'}];
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 module.exports = function(app) {
   app.get("/todo", (req, res) => {
-    res.render('todo', {todos: data});
+    Todo.find({}, function(err, data) {
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
   app.post("/todo", urlencodedParser, (req, res) => {
-    data.push(req.body);
-    res.json(data);
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
   app.delete("/todo/:item", (req, res) => {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 };
